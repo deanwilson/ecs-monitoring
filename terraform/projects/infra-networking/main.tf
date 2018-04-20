625d66d0b2961e72b43b0fb66e1e18a5a38e8ba2
Dean Wilson <dean.wilson@gmail.com>
2018-04-20 11:26:01 +0100

    Expose the public subnet IDS
    These are used by the external load balancers

--- terraform/projects/infra-networking/main.tf
+++ terraform/projects/infra-networking/main.tf
@@ -78,3 +78,8 @@ output "private_subnets" {
   value       = "${module.vpc.private_subnets}"
   description = "List of private subnet IDs"
 }
+
+output "public_subnets" {
+  value       = "${module.vpc.public_subnets}"
+  description = "List of public subnet IDs"
+}
