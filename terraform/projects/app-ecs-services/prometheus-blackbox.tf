38f4cd3c737bde7d4992b04858988b7fbea22500
Dean Wilson <dean.wilson@gmail.com>
2018-05-03 15:06:39 +0100

    Add service registration for prometheus tasks

--- terraform/projects/app-ecs-services/prometheus-blackbox.tf
+++ terraform/projects/app-ecs-services/prometheus-blackbox.tf
@@ -1,5 +1,6 @@
 resource "aws_ecs_task_definition" "prometheus_blackbox" {
   family                = "prometheus-blackbox"
+  network_mode          = "awsvpc"
   container_definitions = "${file("task-definitions/prometheus-blackbox.json")}"
 }
 
@@ -8,4 +9,12 @@ resource "aws_ecs_service" "prometheus_blackbox" {
   cluster         = "${local.cluster_name}"
   task_definition = "${aws_ecs_task_definition.prometheus_blackbox.arn}"
   desired_count   = 1
+
+  service_registries {
+    registry_arn = "${data.terraform_remote_state.infra_service_discovery.service_discovery_arn}"
+  }
+
+  network_configuration {
+    subnets = ["${data.terraform_remote_state.infra_networking.private_subnets}"]
+  }
 }
--- terraform/projects/app-ecs-services/prometheus-service.tf
+++ terraform/projects/app-ecs-services/prometheus-service.tf
@@ -1,5 +1,6 @@
 resource "aws_ecs_task_definition" "prometheus_server" {
   family                = "prometheus-server"
+  network_mode          = "awsvpc"
   container_definitions = "${file("task-definitions/prometheus-server.json")}"
 
   volume {
@@ -18,4 +19,12 @@ resource "aws_ecs_service" "prometheus_server" {
   cluster         = "${local.cluster_name}"
   task_definition = "${aws_ecs_task_definition.prometheus_server.arn}"
   desired_count   = 1
+
+  service_registries {
+    registry_arn = "${data.terraform_remote_state.infra_service_discovery.service_discovery_arn}"
+  }
+
+  network_configuration {
+    subnets = ["${data.terraform_remote_state.infra_networking.private_subnets}"]
+  }
 }
