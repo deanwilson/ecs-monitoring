a77c0bae56644cb26522e62e0a73f486e9870ca2
Dean Wilson <dean.wilson@gmail.com>
2018-05-03 15:03:50 +0100

    Add very basic service discovery stub

---
+++ terraform/projects/infra-service-discovery/main.tf
@@ -0,0 +1,96 @@
+/**
+* ## Project: infra-service-discovery
+*
+* Service discovery
+*
+*/
+
+variable "additional_tags" {
+  type        = "map"
+  description = "Stack specific tags to apply"
+  default     = {}
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "AWS region"
+  default     = "eu-west-1"
+}
+
+variable "remote_state_bucket" {
+  type        = "string"
+  description = "S3 bucket we store our terraform state in"
+  default     = "ecs-monitoring"
+}
+
+variable "remote_state_infra_networking_key_stack" {
+  type        = "string"
+  description = "Override infra-networking remote state path"
+  default     = "infra-security-groups.tfstate"
+}
+
+variable "stack_name" {
+  type        = "string"
+  description = "Unique name for this collection of resources"
+  default     = "ecs-monitoring"
+}
+
+# locals
+# --------------------------------------------------------------
+
+# Resources
+# --------------------------------------------------------------
+
+## Providers
+
+terraform {
+  required_version = "= 0.11.7"
+
+  backend "s3" {
+    key = "infra-service-discovery.tfstate"
+  }
+}
+
+provider "aws" {
+  version = "~> 1.14.1"
+  region  = "${var.aws_region}"
+}
+
+## Data sources
+
+data "terraform_remote_state" "infra_networking" {
+  backend = "s3"
+
+  config {
+    bucket = "${var.remote_state_bucket}"
+    key    = "infra-networking.tfstate"
+    region = "${var.aws_region}"
+  }
+}
+
+
+resource "aws_service_discovery_private_dns_namespace" "prometheus" {
+  name = "sd.ecs-monitoring.com"
+  description = "ECS Monitoring service discovery zone"
+  vpc = "${data.terraform_remote_state.infra_networking.vpc_id}"
+}
+
+
+resource "aws_service_discovery_service" "prometheus" {
+  name = "prometheus-service-discovery"
+
+  dns_config {
+    namespace_id = "${aws_service_discovery_private_dns_namespace.prometheus.id}"
+    dns_records {
+      ttl = 10
+      type = "A"
+    }
+  }
+}
+
+## Outputs
+
+output "service_discovery_arn" {
+  value       = "${aws_service_discovery_service.prometheus.arn}"
+  description = "Service Discovery ARN"
+}
